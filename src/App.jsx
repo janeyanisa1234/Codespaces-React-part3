6ecd544913b27cfb80d00c9b4ecc90b1631b4c66
janeyanisa1234 <129841440+janeyanisa1234@users.noreply.github.com>
2024-10-03 08:38:52 +0000

    react path3

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
 import './App.css';
-
+import { BrowserRouter,Routes,Route,Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Posts from './Posts';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+  <BrowserRouter>
+    <div>
+      <ul>
+        <li><Link to ="/">Home</Link></li>
+        <li><Link to ="/about">About</Link></li>
+        <li><Link to ="/posts?fname=Yanisa&lname=Khonghan">Posts</Link></li>
+        <li><Link to ="/posts/1">Posts id 1</Link></li>
+        <li><Link to ="/posts/2">Posts id 2</Link></li>
+      </ul>
     </div>
+    <Routes>
+      <Route path="/"  element={<Home/>}/>
+      <Route path="/about"  element={<About/>}/>
+      <Route path="/posts"  element={<Posts/>}/>
+      <Route path="/posts/:id"  element={<Posts/>}/>
+
+    </Routes>
+  </BrowserRouter>
   );
 }
 
