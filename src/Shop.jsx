b6dc3861006e42c2ac1095c6942da14684b37835
janeyanisa1234 <129841440+janeyanisa1234@users.noreply.github.com>
2024-10-10 08:20:54 +0000

    fetch product from api

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,5 +1,6 @@
 import './Shop.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 function Item(props){
     return (<div key={props.id} onClick={()=>props.callback(props)}>
         <img src={props.img} width={200} height={200}/><br/>
@@ -9,19 +10,25 @@ function Item(props){
     </div>);
 }
 export default function Shop(){
-    const products=[
-        {id:0,name:"Notebook Acer Swift",price:45900,img:"https://img.advice.co.th/images_nas/pic_product4/A0147295/A0147295_s.jpg"},
-        {id:1,name:"Notebook Asus Vivo",price:19900,img:"https://img.advice.co.th/images_nas/pic_product4/A0146010/A0146010_s.jpg"},
-        {id:2,name:"Notebook Lenovo Ideapad",price:32900,img:"https://img.advice.co.th/images_nas/pic_product4/A0149009/A0149009_s.jpg"},
-        {id:3,name:"Notebook MSI Prestige",price:54900,img:"https://img.advice.co.th/images_nas/pic_product4/A0149954/A0149954_s.jpg"},
-        {id:4,name:"Notebook DELL XPS",price:99900,img:"https://img.advice.co.th/images_nas/pic_product4/A0146335/A0146335_s.jpg"},
-        {id:5,name:"Notebook HP Envy",price:46900,img:"https://img.advice.co.th/images_nas/pic_product4/A0145712/A0145712_s.jpg"}];
-        const [cart,setCart]=useState([]);
-        function addCart(item){
+    const [products,setProducts]=useState([]);
+    const URL ="https://shiny-couscous-4xgqg7pp477hqxv9-5000.app.github.dev";
+    useEffect(()=>{
+        axios.get(URL+'/api/products')
+        .then(response=>{
+            setProducts(response.data);
+        })
+        .catch(error=>{
+            console.log("error");
+        });
+    }
+    ,[]);
+   
+    const [cart,setCart]=useState([]);
+    function addCart(item){
          setCart([...cart,{id:item.id,name:item.name,price:item.price,img:item.img}]);
-        }
-        const productList=products.map(item=><Item {...item} callback={addCart}/>);
-        const cartList=cart.map((item,index)=><li>{item.id} {item.name} {item.price} 
+    }
+    const productList=products.map(item=><Item {...item} callback={addCart}/>);
+    const cartList=cart.map((item,index)=><li>{item.id} {item.name} {item.price} 
         <button onClick={()=>{
             alert('you click'+index)
             setCart(cart.filter((i,_index)=>index!=_index));
